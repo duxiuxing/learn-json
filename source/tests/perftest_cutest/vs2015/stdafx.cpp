04fa07d8cdd65d8408e31c0e8ed128a67f080222
duxiuxing <duxiuxing@foxmail.com>
2018-02-07 18:41:24 +0800

    add test cases to *_cutest.vcxproj

--- source/tests/perftest_cutest/vs2015/stdafx.cpp
+++ source/tests/perftest_cutest/vs2015/stdafx.cpp
@@ -1,8 +1,3 @@
-// stdafx.cpp : source file that includes just the standard includes
-// rapidjson_perftest_cutest.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
+﻿#include "stdafx.h"
 
-#include "stdafx.h"
-
-// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file
+#pragma comment(lib, "cutest.lib")
